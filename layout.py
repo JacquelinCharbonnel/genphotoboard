27f9ed0a9aaf5dec9fe204d766e6b7250f85f73d
Jacquelin Charbonnel <github@jaclin.anonaddy.com>
2025-07-03 08:47:03 +0200

     %

--- layout.py
+++ layout.py
@@ -23,9 +23,15 @@ https://github.com/dimsemenov/Magnific-Popup
 """
 
 import sys, os, importlib
-import board1def
+# import board1def
 import yaml
 from jinja2 import Template
+
+def usage():
+  print(f"""
+usage: {sys.argv[0]} working_dir yml_file
+  """)
+  exit(1)
 
 class Stream:
   def __init__(self):
@@ -45,6 +51,24 @@ class Stream:
 
   def __str__(self):
     return "\n".join(self.stream)
+
+class Style:
+  def __init__(self,name,css):
+    self.name = name
+    self.css = css
+
+  def __enter__(self):
+    s = f".{self.name} {{"
+    self.css << s
+    self.css.inc()
+    return self
+
+  def __exit__(self,type,value,traceback):
+    self.css.dec()
+    self.css << "}\n"
+
+  def get_css(self):
+    return str(self.css)
 
 class Element:
   def __init__(self,name,classes,html,css):
@@ -72,9 +96,10 @@ class Element:
     return str(self.html)
 
 class Html(Element):
-  def __init__(self,templates,html,css):
+  def __init__(self,yml,html,css):
     super().__init__("html",None,html,css)
-    css << Template(templates.css_html).render(board["html"])
+    templates = yml["run"]["templates"]
+    css << Template(templates.css_html).render(yml["html"])
 
 class Head(Element):
   def __init__(self,html):
@@ -82,135 +107,175 @@ class Head(Element):
     # html << Template(templates.html_head)
 
 class Body(Element):
-  def __init__(self,classes,templates,html,css):
+  def __init__(self,classes,yml,html,css):
     super().__init__("body",classes=classes,html=html,css=css)
-    css << Template(templates.css_body).render(board["body"])
+    templates = yml["run"]["templates"]
+    css << Template(templates.css_body).render(yml["body"])
 
 class Main(Element):
-  def __init__(self,classes,templates,html,css):
+  def __init__(self,classes,yml,html,css):
     super().__init__("main",classes,html=html,css=css)
-    css << Template(templates.css_main).render(board["main"])
+    templates = yml["run"]["templates"]
+    css << Template(templates.css_main).render(yml["main"])
 
 class H1(Element):
   def __init__(self,classes,html,css):
     super().__init__("h1",classes=classes,html=html,css=css)
 
+class Ul(Element):
+  def __init__(self,classes,yml,html,css):
+    super().__init__("ul",classes,html=html,css=css)
+    # templates = yml["run"]["templates"]
+    # css << Template(templates.css_ul).render(yml["ul"])
+
+class Li(Element):
+  def __init__(self,classes,yml,html,css):
+    super().__init__("li",classes,html=html,css=css)
+    # templates = yml["run"]["templates"]
+    # css << Template(templates.css_li).render(yml["li"])
+
 class Container(Element):
-  def __init__(self,classes,templates,html,css):
+  def __init__(self,classes,yml,html,css):
     super().__init__("div",["container"],html=html,css=css)
-    css << Template(templates.css_container).render(board["container"])
+    # templates = yml["run"]["templates"]
+    # css << Template(templates.css_container).render(yml["container"])
 
 class Grid(Element):
-  def __init__(self,classes,templates,html,css):
+  def __init__(self,classes,yml,html,css):
     super().__init__("div",["grid"],html=html,css=css)
-    css << Template(templates.css_grid).render(board["grid"])
+    templates = yml["run"]["templates"]
+    css << Template(templates.css_grid).render(yml["grid"])
         
 class Frame(Element):
-  def __init__(self,name,id,classes,templates,html,css):
+  def __init__(self,name,id,classes,yml,html,css):
     super().__init__(name,classes,html,css)
     with Style(f"f{id+1}",css) as st:
       self.css << f"grid-area: f{id+1};"
 
 class Figure(Frame):
-  def __init__(self,name,id,classes,templates,html,css):
-    super().__init__("figure",id,classes,templates,html,css)
-
-    # html << f"""<img src="{board['frame'][id]["img"]}" alt="Board image" class="frame_figure">"""
-    html << Template(templates.html_frame_figure).render({"image": board['frame'][id]["img"]})
+  def __init__(self,name,id,classes,yml,html,css):
+    super().__init__("figure",id,classes,yml,html,css)
+    templates = yml["run"]["templates"]
+    # html << f"""<img src="{yml['frame'][id]["img"]}" alt="yml image" class="frame_figure">"""
+    html << Template(templates.html_frame_figure).render({"image": yml['frame'][id]["img"]})
 
 class Text(Frame):
-  def __init__(self,name,id,classes,templates,html,css):
-    super().__init__("div",id,classes,templates,html,css)
-
-    html << Template(templates.html_frame_text).render({"text": board['frame'][id]["text"]})
-    # html << f"""{board['frame'][id]["text"]}"""
-
-class Style:
-  def __init__(self,name,css):
-    self.name = name
-    self.css = css
-
-  def __enter__(self):
-    s = f".{self.name} {{"
-    self.css << s
-    self.css.inc()
-    return self
-
-  def __exit__(self,type,value,traceback):
-    self.css.dec()
-    self.css << "}\n"
-
-  def get_css(self):
-    return str(self.css)
+  def __init__(self,name,id,classes,yml,html,css):
+    super().__init__("div",id,classes,yml,html,css)
+    templates = yml["run"]["templates"]
+    html << Template(templates.html_frame_text).render({"text": yml['frame'][id]["text"]})
+    # html << f"""{yml['frame'][id]["text"]}"""
 
 class Page:
-  def __init__(self):
-    pass
-
-class BoardPage(Page):
-  def __init__(self,board):
-    self.board = board
-
-  def render(self): 
-    board = self.board
-
-    template_file = board["template_file"]
+  def __init__(self,yml):
+    self.yml = yml
+    template_file = yml["template_file"]
     print(template_file)   
     template_spec = importlib.util.spec_from_file_location("templates", template_file)
     templates = importlib.util.module_from_spec(template_spec)
     sys.modules["templates"] = templates
     template_spec.loader.exec_module(templates)
+    yml["run"] = {}
+    yml["run"]["templates"] = templates
+
+class BoardPage(Page):
+  def __init__(self,yml):
+    super().__init__(yml)
+
+  def render(self): 
+    yml = self.yml
+    templates = yml["run"]["templates"]
 
     html_complete = Stream()
     html_body = Stream()
     css = Stream()
 
-    css << Template(templates.css_glob).render(board["glob"])
-    css << Template(templates.css_modale).render(board["modale"])
+    css << Template(templates.css_glob).render(yml["glob"])
+    css << Template(templates.css_modale).render(yml["modale"])
+    css << Template(templates.css_container).render(yml["container"])
 
-    with Body(classes=[],templates=templates,html=html_body,css=css):
-      with Main(classes=[],templates=templates,html=html_body,css=css):
+    with Body(classes=[],yml=yml,html=html_body,css=css):
+      with Main(classes=[],yml=yml,html=html_body,css=css):
       #  with H1(classes=[],html=html_body,css=css) as h1:
       #   h1.html << "mon titre"
-       with Container(classes=[],templates=templates,html=html_body,css=css) as c:
-        with Grid(classes=[],templates=templates,html=html_body,css=css) as g:
-          css << Template(templates.css_frame_figure).render(board["frame_figure"])
-          css << Template(templates.css_frame_text).render(board["frame_text"])
-          for (i,f) in enumerate(board["frame"]) :
+       with Container(classes=[],yml=yml,html=html_body,css=css) as c:
+        with Grid(classes=[],yml=yml,html=html_body,css=css) as g:
+          css << Template(templates.css_frame_figure).render(yml["frame_figure"])
+          css << Template(templates.css_frame_text).render(yml["frame_text"])
+          for (i,f) in enumerate(yml["frame"]) :
             # with Element(f["elt"],classes=[f"f{i}"]) as elt:
-            with eval(f["elt"])(name=f"f{i+1}",id=i,classes=[f"f{i+1}"],templates=templates,html=html_body,css=css) as elt:
+            with eval(f["elt"])(name=f"f{i+1}",id=i,classes=[f"f{i+1}"],yml=yml,html=html_body,css=css) as elt:
               pass
 
       html_body << Template(templates.html_modale).render({})
 
     html_complete << "<!DOCTYPE html>"
-    with Html(html=html_complete,templates=templates,css=css):
+    with Html(html=html_complete,yml=yml,css=css):
       with Head(html=html_complete):
         html_complete << Template(templates.html_head).render({"inline_style": css})
       html_complete << html_body  
 
-    # html_file = os.path.join(board_dir,"index.html")
-    with open(board["output_file"],"w") as f:  
+    # html_file = os.path.join(yml_dir,"index.html")
+    with open(yml["output_file"],"w") as f:  
       print(html_complete, file=f)
 
     print(html_complete)
 
+class ThumbnailPage(Page):
+  def __init__(self,yml):
+    super().__init__(yml)
+
+  def render(self): 
+    yml = self.yml
+    templates = yml["run"]["templates"]
+
+    html_complete = Stream()
+    html_body = Stream()
+    css = Stream()
+
+    css << Template(templates.css_glob).render(yml["glob"])
+    # css << Template(templates.css_modale).render(yml["modale"])
+    css << Template(templates.css_container).render(yml["container"])
+    css << Template(templates.css_ul).render(yml["ul"])
+    css << Template(templates.css_li).render(yml["li"])
+    css << Template(templates.css_img).render(yml["img"])
+    css << Template(templates.css_frame_figure).render(yml["frame_figure"])
+    css << Template(templates.css_frame_text).render(yml["frame_text"])
+
+    with Body(classes=[],yml=yml,html=html_body,css=css):
+     with Container(classes=[],yml=yml,html=html_body,css=css) as c:
+      with Ul(classes=[],yml=yml,html=html_body,css=css) as ul:
+          for (i,f) in enumerate(yml["frame"]) :
+            # with Element(f["elt"],classes=[f"f{i}"]) as elt:
+           with Li(classes=[],yml=yml,html=html_body,css=css) as li:
+            with eval(f["elt"])(name=f"f{i+1}",id=i,classes=[f"f{i+1}"],yml=yml,html=html_body,css=css) as elt:
+              pass
+
+      # html_body << Template(templates.html_modale).render({})
+
+    html_complete << "<!DOCTYPE html>"
+    with Html(html=html_complete,yml=yml,css=css):
+      with Head(html=html_complete):
+        html_complete << Template(templates.html_head).render({"inline_style": css})
+      html_complete << html_body  
+
+    # html_file = os.path.join(yml_dir,"index.html")
+    with open(yml["output_file"],"w") as f:  
+      print(html_complete, file=f)
+
+    print(html_complete)
 
 if __name__=="__main__":
 
   if len(sys.argv)!=3:
-    print(f"""
-usage: {sys.argv[0]} working_dir board_name
-    """)
-    exit(1)
-  board_dir = sys.argv[1]
-  board_name = sys.argv[2]
-  board_def = os.path.join(board_dir,board_name)
+    usage()
 
-  board = yaml.load(open(board_def,"r"),Loader=yaml.CLoader)
+  dir = sys.argv[1]
+  ymlfn = sys.argv[2]
 
-  os.chdir(board_dir)
+  os.chdir(dir)
 
-  page = BoardPage(board)
+  yml = yaml.load(open(ymlfn,"r"),Loader=yaml.CLoader)
+  page = eval(yml["class"])(yml)
   page.render()
   
